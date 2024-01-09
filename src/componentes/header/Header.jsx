af5018a681efa4ea4913c70c86cfc50af91129f3
Manoel <piloukos45@gmail.com>
2024-01-09 17:06:04 -0300

    teste leyaut

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -4,27 +4,33 @@ import { useAuthContext } from "../../contexts/AuthContext"
 
 export const Header = () => {
 
-    const {hendleLogin, hendleToogle, toogle } = useAuthContext()
+    const {hendleLogin, hendleToogle, toogle, auths } = useAuthContext()
     const get_produtos = JSON.parse(localStorage.getItem("produtos"))
     const  filterCarrinho = get_produtos ?  get_produtos.filter((e)=> e.contador > 0 ) : ""
   
 
     return (
         <header>
-            
+            <section className={styles.banner}>
+                
+            </section>
             <section className={styles.area_toogle}>
                 <div 
                     className={styles.toogle}
                     onClick={() => hendleToogle( toogle === "-310px" ? "0px" : "-310px")}
                 ></div>
                 <p onClick={()=> hendleLogin()}>Sair</p>
-                <div className={styles.carrinho}>
-                    <span>{filterCarrinho.length}</span>
-                </div>
+                { !auths && 
+                    (
+                        <div className={styles.carrinho}>
+                            <span
+                                style={{color:filterCarrinho.length > 0 ? "#73ff00" : "tomato"}}
+                            >{filterCarrinho.length}</span>
+                        </div>
+                    )
+                }
             </section>
-            <section className={styles.banner}>
-                <h2>Kbana Drinks</h2>
-            </section>
+        
   
         </header>
     )

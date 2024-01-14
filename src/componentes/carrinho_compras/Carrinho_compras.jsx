76d4d9bd5a606dec41716e2ea9c2371805f2cf56
Manoel <piloukos45@gmail.com>
2024-01-14 20:30:19 -0300

    jfeihfji

--- src/componentes/carrinho_compras/Carrinho_compras.jsx
+++ src/componentes/carrinho_compras/Carrinho_compras.jsx
@@ -64,7 +64,7 @@ export const Carrinho_compras = () => {
 
       const getInfoUser = JSON.parse(localStorage.getItem("UserName"))
 
-      const headerText = `🥂______*KBANA DRINKs*______🥂\n\n*Cardapio:* https://main--classy-conkies-01e448.netlify.app/\n\nNome: *${getInfoUser}*\nLocalização: ${getLocalizacao}\n\nPedido: *${dataHoraFormatada}*\n`
+      const headerText = `*Cardapio:* https://main--classy-conkies-01e448.netlify.app/\n\n🥂________*KBANA DRINKs*_________🥂\n\nNome: *${getInfoUser}*\nLocalização: ${getLocalizacao}\n\nPedido: *${dataHoraFormatada}*\n`
       
      
    
